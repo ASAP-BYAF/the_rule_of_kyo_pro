15875367a51a791bc1ae3de9e16784b1638a2947
teppei <teppei0830.4869@gmail.com>
2023-05-08 22:11:05 +0900

    13.py を修正しました。
    入力例１に対して出力は正しい値を得ます。
    しかし、実際に提出すると、ほとんどが不正解になります。
    今後、修正が必要です。

--- 13.py
+++ 13.py
@@ -1,7 +1,7 @@
 def bi_search_func(x, target):
     """
-    与えられた整数配列 x の中から整数 target 以上の整数のうち
-    最小のものの配列の引数を返します。
+    与えられた整数配列 x の中から整数 target 以下の整数のうち
+    最大のものの配列の引数を返します。
 
     :param x:
     :param target:
@@ -10,39 +10,39 @@ def bi_search_func(x, target):
     x_sorted = sorted(x)
     left = 0
     right = len(x) - 1
-    counter_min_index = right + 1
+    counter_min_index = 0
 
     while left <= right:
         mid = (left + right) // 2
 
-        if x_sorted[mid] >= target:
-            counter_min_index = min(counter_min_index, mid)
+        if x_sorted[mid] > target:
             # print('if')
             # print(f'counter_min_index = {counter_min_index}')
             # print(f'left = {left}')
             # print(f'right = {right}')
             right = mid - 1
 
-        elif x_sorted[mid] < target:
+        elif x_sorted[mid] <= target:
+            counter_min_index = max(counter_min_index, mid)
             # print('else')
             # print(f'counter_min_index = {counter_min_index}')
             # print(f'left = {left}')
             # print(f'right = {right}')
             left = mid + 1
 
-    # print(counter_min_index)
-    # print(x)
-    # print(target, x[counter_min_index])
+    print(counter_min_index)
+    print(x)
+    print(target, x[counter_min_index])
     return counter_min_index
 
 n, k = map(int, input().split())
 num_list = list(map(int, input().split()))
 
 total_num_combi = 0
-for i, i_num in enumerate(num_list[:-1],1):
+for i, i_num in enumerate(num_list[:-1]):
     k_counter = i_num + k
     # print(f' >>> k = {k}, k_counter = {k_counter}')
-    total_num_combi += ((n - i) - (bi_search_func(num_list[i:], k_counter) + 1) + 1)
+    total_num_combi += (bi_search_func(num_list[i+1:], k_counter) + 1)
     # print('total_tmp', total_num_combi)
 print(total_num_combi)
 
