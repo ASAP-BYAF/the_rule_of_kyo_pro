be0cb28310dee2a541c7f24c254af9332601c145
teppei <teppei0830.4869@gmail.com>
2023-06-03 15:49:28 +0900

    19.py を修正しました。
    教科書を見本にして、書き直しました。

--- 19.py
+++ 19.py
@@ -5,17 +5,15 @@ for _ in range(n):
 
 w_max = w+1
 n_max = n+1
-dp = [[0 for _ in range(w_max)] for _ in range(n_max)]
+# dp = [[0 for _ in range(w_max)] for _ in range(n_max)]
+dp = [[-1.0e+10 for _ in range(w_max)] for _ in range(n_max)]
+dp[0][0] = 0
 
 for j, (j_w, j_v) in enumerate(item_list):
     for i, i_dp in enumerate(dp[j]):
-        if i_dp :
-            if i+j_w < w_max:
-                dp[j+1][i+j_w] = max(dp[j][i+j_w], dp[j][i] + j_v)
-            dp[j+1][i] = max(dp[j+1][i], dp[j][i])
-    dp[j+1][j_w] = max(dp[j+1][j_w], j_v)
+        if i < j_w: 
+            dp[j+1][i]= dp[j][i]
+        else:
+            dp[j+1][i] = max(dp[j][i], dp[j][i-j_w] + j_v)
 
-for i in dp[n][::-1]:
-    if i:
-        print(i)
-        break
+print(max(dp[n]))
