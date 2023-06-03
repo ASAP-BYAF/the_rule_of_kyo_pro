67ee9fbd2c9de3bb726097ace19ac0e717c3265c
teppei <teppei0830.4869@gmail.com>
2023-06-03 13:46:37 +0900

    18.py を修正しました。
    教科書を見本にして書きました。

--- 18.py
+++ 18.py
@@ -1,16 +1,19 @@
-import sys
-
 n, s = map(int, input().split())
 a_list = list(map(int, input().split()))
 
-sum_set = {0}
-for a_i in a_list:
-    sum_set_tmp = set()
-    for sum_i in sum_set:
-        sum_set_tmp.add(sum_i + a_i)
-    sum_set = sum_set | sum_set_tmp
+dp = [[False for _ in range(10001)] for _ in range(61)]
+dp[0][0] = True
+
+for i, a_i in enumerate(a_list):
+    for j, sum_j in enumerate(dp[i]):
+        if sum_j:
+            dp[i+1][j] = True
+            if j+a_i <=10000:
+                dp[i+1][j+a_i] = True
     
-    if s in sum_set:
-        print('Yes')
-        sys.exit()
-print('No')
+if dp[n][s]:
+    print('Yes')
+else:
+    print('No')
+
+
