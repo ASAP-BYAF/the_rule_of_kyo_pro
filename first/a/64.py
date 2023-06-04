622622fd53bc90e1b982c912f958af8305b9d7b2
teppei <teppei0830.4869@gmail.com>
2023-06-04 22:01:17 +0900

    64.py を修正しました。
    距離の配列 dist から探索した頂点を探すのにかかる計算時間を削減するために、
    探索済みの頂点の番号を格納する配列 v_visited を作成しました。
    しかし、まだ、実行事件制限超過になります。今後修正が必要です。

--- first/a/64.py
+++ first/a/64.py
@@ -7,16 +7,18 @@ for _ in range(m):
 
 dist = [1.0e+10 for i in range(n+1)]
 dist[1] = 0
+v_visited = [1]
 
 while True:
     min_dist = 1.0e+20
-    for i, i_dist in enumerate(dist[1:], 1):
-        if i_dist < 1.0e+9:
-            for v, w in graph[i]:
-                if dist[v] > 1.0e+9:
-                    if i_dist + w < min_dist:
-                        min_dist = i_dist + w
-                        min_v = v
+    for i in v_visited:
+        for v, w in graph[i]:
+            if dist[v] > 1.0e+9:
+                dist_nex = dist[i] + w
+                if dist_nex < min_dist:
+                    min_dist = dist_nex
+                    min_v = v
+                    v_visited.append(min_v)
     if min_dist > 1.0e+15:
         break
     else:
